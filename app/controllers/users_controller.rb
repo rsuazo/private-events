e95e40c5932f7b0c8cc748c31844b21b1c81a4e2
rsuazo <rsuazo2@gmail.com>
2020-06-25 11:01:16 -0700

    create simple 'new', 'create', & 'show' methods in user controller and corresponding views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.create(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
 
+    redirect_to @user
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 end

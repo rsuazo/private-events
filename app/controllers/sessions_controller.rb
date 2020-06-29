953c2c308443b055b9af63b854cfcd61d2e88a02
rsuazo <rsuazo2@gmail.com>
2020-06-29 13:02:26 -0700

    complete simple sign in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to user_path, notice: "Logged in!"
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice: "Logged in!"
     else
       flash.now.alert = "Email or password is invalid"
       render "new"
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    redirect_to users_url, notice: "Logged out!"
+    redirect_to login_path, notice: "Logged out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root to: 'sessions#new'
+  
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'

7d600cc476a715ce52451aa7bd0ad8686c63f2d5
rsuazo <rsuazo2@gmail.com>
2020-07-10 14:14:27 -0700

    update user model with has many through relationship to event_attendings table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
+  
+  has_many :event_attendings, foreign_key: :event_attendee_id
+  has_many :attended_events, through: :event_attendings
+
+
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event"
 end

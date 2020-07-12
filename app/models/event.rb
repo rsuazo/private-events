96ee40c0d6e0e7f0517b190f6549390d4c226f9d
rsuazo <rsuazo2@gmail.com>
2020-07-11 20:11:32 -0700

    saving changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
-  has_many :event_attendees, foreign_key: :attended_event_id
-  has_many :attendees, through: :event_attendees, source: event_attendee
+  has_many :event_attendings, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendings, source: :event_attendee
+  
+  
   belongs_to :creator, class_name: "User"
 end

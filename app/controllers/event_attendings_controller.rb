72f955f60a787a065a203662526511bbab96656a
rsuazo <rsuazo2@gmail.com>
2020-07-14 18:59:25 -0700

    update user model to fix event_attendings association

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -4,10 +4,9 @@ class EventAttendingsController < ApplicationController
   end
 
   def create
+    @event = Event.find(params[:event_id])
 
     @event_attending = EventAttending.new(event_attendee_id: params[:user_id], attended_event_id: params[:event_id])
-    @event = Event.find(params[:event_id])
-    @user = User.find(params[:user_id])
 
     if @event_attending.save
       redirect_to @event, notice: "Event attended!"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   
-  has_many :event_attendings, foreign_key: :attended_event_id
+  has_many :event_attendings, foreign_key: :event_attendee_id
   has_many :attended_events, through: :event_attendings
-
 
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
 end
